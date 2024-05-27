49f9ecdc50efb682e1672bb30617bf64d52b351f
shixiuhai <15256728901@163.com>
2024-05-27 21:57:08 +0800

    更新了代码

--- redis_dict/demo.py
+++ redis_dict/demo.py
@@ -1,41 +1,53 @@
 import redis
 import json
 class RedisDict:
-    def __init__(self, key, redis_host='localhost', redis_port=6379,redis_password=None):
+    def __init__(self, key, redis_host='localhost', redis_port=6379, redis_password=None):
+        # 初始化方法，创建一个 Redis 连接对象
         self.redis = redis.StrictRedis(
             host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+        # 指定 Redis 哈希表的键
         self.key = key
 
     def __getitem__(self, item):
+        # 获取指定键的值
         value = self.redis.hget(self.key, item)
         if value is not None:
+            # 解码存储的 JSON 字符串并返回
             return json.loads(value)
         else:
             raise KeyError(item)
 
     def __setitem__(self, item, value):
+        # 将值转换为 JSON 格式并设置指定键的值
         serialized_value = json.dumps(value)
         self.redis.hset(self.key, item, serialized_value)
 
     def __delitem__(self, item):
+        # 删除指定键
         self.redis.hdel(self.key, item)
 
     def __len__(self):
+        # 返回哈希表的大小（键值对数量）
         return self.redis.hlen(self.key)
 
     def __iter__(self):
+        # 返回一个迭代器，用于迭代所有键
         return iter(self.keys())
 
     def keys(self):
+        # 返回所有键
         return self.redis.hkeys(self.key)
 
     def values(self):
+        # 返回所有值
         return [json.loads(value) for value in self.redis.hvals(self.key)]
 
     def items(self):
+        # 返回所有键值对
         return [(key, json.loads(value)) for key, value in self.redis.hgetall(self.key).items()]
 
     def clear(self):
+        # 清空哈希表
         self.redis.delete(self.key)
 
 # # 设置键值对
@@ -84,6 +96,7 @@ if __name__ == "__main__":
         "warnStatus": 2
     }
     task_item=taskResults["task_id1"]
+    task_item["warnStatus"]=1
     task_item["warnData"].append({
         "a":1,
         "b":3
